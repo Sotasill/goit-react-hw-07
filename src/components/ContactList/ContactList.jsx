d5d28c14e6f7dc278c9c128fd57fa3e881a3444e
SotaSill <sotasill@gmail.com>
2024-11-06 20:08:30 +0200

    without filter

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,30 +2,18 @@ import Contact from "../Contact/Contact";
 import css from "./ContactList.module.css";
 import { useSelector } from "react-redux";
 import { selectFilteredContacts } from "../../redux/contactsSlice";
-import { selectNameFilter } from "../../redux/filtersSlice";
 
 function ContactList() {
-    const contacts = useSelector(selectFilteredContacts)
-     const filter = useSelector(selectNameFilter);
-
-  const visibleContacts = contacts.filter((contact) =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
+  // Получение уже отфильтрованных контактов из селектора
+  const contacts = useSelector(selectFilteredContacts);
 
   return (
     <ul className={css.contactBox}>
-      {visibleContacts.map((contact) => (
-        <Contact
-          key={contact.id}
-          data={contact}
-        />
+      {contacts.map((contact) => (
+        <Contact key={contact.id} data={contact} />
       ))}
     </ul>
   );
 }
 
 export default ContactList;
-
-
-
-
